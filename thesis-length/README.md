153a10985f8500705fa18b384b7c0c25a005f059
Charlie Jacomme <charlie.jacomme@inria.fr>
2025-11-09 21:05:57 +0100

    thesis-lengthj experiment

---
+++ thesis-length/README.md
@@ -0,0 +1,71 @@
+# Is the length of the PhD thesis manuscript biased by gender in French computer science?
+
+TLDR: probably, on average, women writes a 10% longer thesis. (but this gap disappears at the HDR level, see ![hdr-length-results](../hdr-length/README.md)
+
+
+The script in this folder automatically found the length of the French PhDs in computer science from 2015 to 2025. The two distrubtions are as follows.
+
+Total number of PhDs successfully scraped: 10462 male and 4110 female.
+
+
+## Number of pages of French thesis in computer sciences, by gender, from 2015 to 2025
+```
+Mean (Male): 193.07790097495698
+Mean (Female): 214.21922141119222
+Median (Male): 173.0
+Median (Female): 181.0
+Standard dev (Male): 87.41048539209191
+Standard dev (Female): 119.22316831939166
+
+TtestResult(statistic=np.float64(-11.785049774883824), pvalue=np.float64(6.50745349356456e-32), df=np.float64(14570.0))
+KstestResult(statistic=np.float64(0.07327466195583965), pvalue=np.float64(3.177214593301979e-14), statistic_location=np.int64(218), statistic_sign=np.int8(1))
+```
+
+The density functions are as follows, with male distribution in blue and female distribution in orange. We observe a clear over abundance for females in the range 300-500 pages.
+
+![density functions for the length of PhDs by gender ](thesis-length-distribution-M-blue-W-orange.png)
+
+
+## Limitations
+
+The gender of each author is *approximated* and might be incorrect. The approximation is based on a French database of names, each being associated to its majority gender representation. This is of course incorrect, and further name does not have to be linked with gender. Yet, in the current society, this enables to get an overall valid estimation. To note, there is also a bias, as some foreign names not contained in the database where excluded from the analysis (2078 entries).
+
+However, for french authors, the estimation should be rather accurate.
+
+
+The python script was implemented as a one shot thing, to get quick results. 
+
+
+## Evolution?
+
+By splitting the 10 year period into two, one does not see a very significant evolution w.r.t. the bias. Thesis are getting *slightly* shorter on average, losing 5 or 3 pages. Curiously, the standard devition for males is reducing, while it is increasing for females.
+
+### 2015-2029
+
+```
+Mean (Male): 195.47825285661628
+Mean (Female): 215.4007336084365
+Median (Male): 174.0
+Median (Female): 181.0
+Standard dev (Male): 92.43607082825044
+Standard dev (Female): 113.79406530196886
+
+TtestResult(statistic=np.float64(-7.933647729408499), pvalue=np.float64(2.432214896811569e-15), df=np.float64(7605.0))
+KstestResult(statistic=np.float64(0.06774335129328739), pvalue=np.float64(1.179184712249173e-06), statistic_location=np.int64(294), statistic_sign=np.int8(1))
+```
+
+### 2020-2025
+
+```
+Mean (Male): 190.49166004765686
+Mean (Female): 212.88335925349924
+Median (Male): 173.0
+Median (Female): 181.0
+Standard dev (Male): 81.5712393366595
+Standard dev (Female): 125.06455099640182
+
+TtestResult(statistic=np.float64(-8.744398928392792), pvalue=np.float64(2.7765903234642577e-18), df=np.float64(6963.0))
+KstestResult(statistic=np.float64(0.08326837850936193), pvalue=np.float64(7.289572542072959e-09), statistic_location=np.int64(229), statistic_sign=np.int8(1))
+```
+
+
---
+++ thesis-length/count.py
@@ -0,0 +1,221 @@
+# thesis file extracted from 
+# https://api.archives-ouvertes.fr/search/?q=*:*&fq=docType_s:THESE&fq=domain_t:info&fq=submittedDateY_i:[2015%20TO%202020]&fl=defenseDateY_i&fl=files_s&fl=authLastName_s&fl=authFirstName_s&rows=20000
+# https://api.archives-ouvertes.fr/search/?q=*:*&fq=docType_s:THESE&fq=domain_t:info&fq=submittedDateY_i:[2021%20TO%20*]&fl=defenseDateY_i&fl=files_s&fl=authLastName_s&fl=authFirstName_s&rows=20000
+
+# importing PyPDF2 library
+
+import PyPDF2
+
+import requests
+from io import BytesIO
+import json
+import csv
+import statistics 
+
+
+# some invalid urls failing
+invalid_urls = ['https://theses.hal.science/tel-02117886/file/Drieu_la_Rochelle_Armand_2018_ED414.pdf',
+                'https://hal.science/tel-04471768/file/These_Nadir_Cherifi%20%281%29.pdf',
+                'https://ubs.hal.science/tel-05025573/file/2012theseDuarteK.pdf',
+                'https://hal.science/tel-04861440/file/Doctorat%20John%20Kingston%20%281%29.jpg',
+                'https://theses.hal.science/tel-04709062/file/122987_GARNIER_2024_archivage.pdf',
+                'https://theses.hal.science/tel-05033245/file/TH2024GIAMPICCOLOCAMILLE.pdf',
+                'https://ubs.hal.science/tel-03105715/file/MushtaqM_these_remaniee.pdf',
+                'https://hal.science/tel-04935803/file/GARCIA_MARTINEZ.pdf',
+'https://enac.hal.science/tel-02453297/file/171115_VOUNDY_125qrkfd43qpml681ycyunl870whiuxh_TH.pdf',
+'https://normandie-univ.hal.science/tel-02552250/file/ManuscritSamuelBovee.pdf',
+'https://univ-pau.hal.science/tel-02437314/file/Final%20Thesis.pdf',
+'https://univ-pau.hal.science/tel-02437294/file/AbdelTh%C3%A9se.pdf',
+'https://hal.inrae.fr/tel-02841288/file/MLTAUPIN_1.pdf',
+'https://hal.sorbonne-universite.fr/tel-02641309/file/these_katz.pdf',
+'https://hal-lirmm.ccsd.cnrs.fr/tel-02418022/file/tese_tex_Jan16-02.pdf',
+'https://univ-pau.hal.science/tel-02437337/file/Le%20memoire%20-%20latex.pdf',
+'https://univ-pau.hal.science/tel-02437332/file/Final%20Thesis.pdf',
+'https://univ-pau.hal.science/tel-02437303/file/th%C3%A8se%2001-02-2018.pdf',
+'https://univ-pau.hal.science/tel-02437343/file/these_MK-final.pdf',
+'https://theses.hal.science/tel-04731754/file/241007_ManuscritThese_CBolut_vfinale.pdf',
+'https://theses.hal.science/tel-04521335/file/127283_ATHEUPE_GATCHEU_2023_archivage.pdf',
+'https://hal.science/tel-05293793/file/thesis_Hohnadel.pdf',
+'https://hal-lirmm.ccsd.cnrs.fr/tel-04301030/file/8.%20Tese%20Final_DSC_Heraldo.pdf',                
+                'https://institut-agro-dijon.hal.science/tel-04186641/file/THESE1993%20MH%20GRAS%20micriobiologie%20salades.pdf',
+                'https://hal.inrae.fr/tel-02787502/file/These_ECREPONT_Stephane2019_1']
+
+def get_pages(url):
+  if not url in invalid_urls:
+    try:
+      print("'" + url+"',")
+      file = BytesIO(requests.get(url).content)
+      # opened file as reading (r) in binary (b) mode
+      # file = open('https://theses.hal.science/tel-00321615/file/Nguyen.Gia-Toan_1986_these.pdf',            'rb')
+      # store data in pdfReader
+      pdfReader = PyPDF2.PdfReader(file)
+      # count number of pages
+      totalPages = len(pdfReader.pages)
+      # print number of pages
+      return(totalPages)
+    except PyPDF2.errors.PdfReadError:
+       return None
+
+with open('api.archives-ouvertes.fr_2015_2020.json', 'r') as file:
+    data1 = json.load(file)
+
+with open('api.archives-ouvertes.fr_2020_2025.json', 'r') as file:
+    data2 = json.load(file)
+    
+
+names = {}
+
+numbers = {}
+
+
+# name list from INSEE
+with open('prenoms-2024-liste.csv', 'r', encoding='utf-8', newline='') as name_file:
+    name_reader = csv.reader(name_file, delimiter=';')
+    for row in name_reader:
+      name=row[1].lower()
+      g=row[0]
+      number=row[3]
+      if name in names:
+          # if name is less common, we ignore and skip
+          if number<numbers[name]:
+              continue
+          
+      # we initialize values for a new name    
+      if g=='1':
+         names[name]="H"
+         numbers[name]=number
+      elif g=='2':
+         names[name]="F"
+         numbers[name]=number         
+
+# print(names)
+    
+def get_gender(name):
+    """Prend chaque prénom individuellement sans prendre en compte le nom de famille"""
+    # Convertir le nom en minuscules
+    name_lower = name.lower()
+
+    # Diviser le nom en parties en utilisant l'espace comme séparateur
+    name_parts = name.split()
+
+    for part_name in [part.lower() for part in name_parts[0:]]:
+        if part_name in names:
+            return names[part_name]
+
+        
+    return None
+
+
+# A dictionnary, indexed by full name. for each key,  contains a sub list "Gender"  
+results = {}
+
+# this is stored in a csv: full name; gender; anne; url
+
+with open('results.csv', 'r', encoding='utf-8', newline='') as result_file:
+    result_reader = csv.reader(result_file, delimiter=';')
+    for row in result_reader:
+        results[row[0]] = {"gender":row[1], "year":row[2], "url":row[3], "pages":row[4]}
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+        
+def print_res(cond):
+
+  H_pages = []
+  F_pages = []
+  None_pages = []
+
+  
+  for result in results:
+    result=results[result]
+    if result["gender"] == "H" and cond(int(result['year'])) :
+        H_pages += [int(result["pages"])]
+    elif result["gender"] == "F"  and cond(int(result['year'])):
+        F_pages += [int(result["pages"])]
+    else:
+        None_pages += [int(result["pages"])]
+
+            
+  
+  counter_H = len(H_pages)
+  counter_H_pages = sum(H_pages)
+  
+  counter_F = len(F_pages)
+  counter_F_pages = sum(F_pages)
+  
+  median_H =  statistics.median(H_pages)
+  median_F = statistics.median(F_pages)
+  dev_H = statistics.pstdev(H_pages)
+  dev_F = statistics.pstdev(F_pages)
+  
+  
+  print("Counter H :"+str(counter_H)+" Counter H pages :"+str(counter_H_pages))
+  print("Counter F :"+str(counter_F)+" Counter F pages :"+str(counter_F_pages) )
+  print("Moyenne H :" + str(counter_H_pages/counter_H))
+  print("Moyenne F :" + str(counter_F_pages/counter_F))
+  print("Medianne H :" + str(median_H))
+  print("Medianne F :" + str(median_F))
+  print("Dev H :" + str(dev_H))
+  print("Dev F :" + str(dev_F))
+  
+  
+  from scipy import stats
+  print(stats.ttest_ind(H_pages, F_pages))
+  print(stats.ks_2samp(H_pages, F_pages))
+
+  return(H_pages, F_pages)
+
+
+
+count=0
+print("Checking entries: "+str(len(data1['response']['docs']+data2['response']['docs'])))
+for entry in data1['response']['docs']+data2['response']['docs']:
+    # we only keep entries with a single file
+    # we lose ~
+    if 'files_s' in entry and len(entry['files_s'])==1:
+        nom=entry['authLastName_s'][0]
+        prenom=entry['authFirstName_s'][0]
+        url=entry['files_s'][0]
+        date=entry['defenseDateY_i']           
+        fullname = prenom+" "+nom
+        if not fullname in results:
+            pages=get_pages(url)
+            if pages:
+                outfile = open('results.csv', 'a')                   
+                outfile.write(';'.join(str(i) for i in [fullname, get_gender(prenom), date, url, pages])+"\n")
+            else:
+              count +=1                
+    else:
+      count +=1
+
+print("Ignored invalid entries: " + str(count))
+
+None_pages = []
+
+  
+for result in results:
+  result=results[result]
+  if result["gender"] == "None": 
+        None_pages += [int(result["pages"])]
+
+
+print("Unknown gender (mostly due to non French surnameS) :"+str(len(None_pages)))
+
+
+print("Stats for 2015 - 2019")
+print_res( (lambda x : x < 2020))
+print(" ")
+print("Stats for 2020 - 2025")
+print_res( (lambda x :  2020 <= x))
+print(" ")
+print("Stats for 2015 - 2025")
+H,F = print_res( (lambda x : True))
+
+plt.hist(H,bins=50,density=True,histtype="step")
+plt.hist(F,bins=50,density=True,histtype="step")  
+
+
+plt.show()
+
+
